2a9f0949a7adcd314d7fa12151e8cb9ff34065bc
Kawas <ahmed.alkawas77@gmail.com>
2025-07-21 21:48:44 +0300

    First commit

---
+++ app/src/main/java/com/sahl/tracking/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sahl.tracking
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.sahl.tracking.ui.theme.SahlTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SahlTheme {
+
+            }
+        }
+    }
+}
